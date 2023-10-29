ab062c019f69ff4bb7995518b25950bf4db43e41
farellano <francesc.arellano@qgenomics.com>
2023-10-29 19:19:56 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -86,17 +86,6 @@ $(window).scroll(function() {
     },
   });
   
-  // Cache the mobile-dnone element
-  var mobileDNone = $(".mobile-dnone");
-
-  $(window).scroll(function() {
-      var scrollPos = $(this).scrollTop();
-      var elementOffset = mobileDNone.offset().top;
-      
-      if (scrollPos > elementOffset) {
-          mobileDNone.removeClass("mobile-dnone");
-      }
-  });
   $(".owl-carousel1").owlCarousel({
     loop: true,
     center: true,
@@ -121,3 +110,16 @@ $(window).scroll(function() {
   });
 
 })(jQuery);
+
+$(document).ready(function() {
+  var mobileDNone = $(".mobile-dnone"); // Define mobileDNone correctamente
+  
+  $(window).scroll(function() {
+      var scrollPos = $(this).scrollTop();
+      var elementOffset = mobileDNone.offset().top;
+
+      if (scrollPos > elementOffset) {
+          mobileDNone.removeClass("mobile-dnone");
+      }
+  });
+});
